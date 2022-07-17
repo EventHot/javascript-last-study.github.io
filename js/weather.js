8028236c950007b92396b7bcf1d931d52d62ddc6
LEEJONGHYUN <admin@eventhot.com>
2022-07-17 23:19:22 +0900

    bug fix

--- js/weather.js
+++ js/weather.js
@@ -7,7 +7,8 @@ const API_KEY = "18adbc1da5558b3f2f949d3fa5e142ae";
 const weatherImage = {
   Rain: "https://cdn-icons.flaticon.com/png/512/2112/premium/2112368.png?token=exp=1658050305~hmac=65b3b7153d6539430d08d103836b86c9",
   Clear: "https://cdn-icons-png.flaticon.com/512/3093/3093303.png",
-  snow: "https://cdn-icons-png.flaticon.com/512/1163/1163629.png",
+  Snow: "https://cdn-icons.flaticon.com/png/512/2315/premium/2315309.png?token=exp=1658068424~hmac=bf1aad7596532dd1a317481445b71ad4",
+  Clouds: "https://cdn-icons-png.flaticon.com/512/414/414825.png",
 };
 
 function onGeoOk(position) {
